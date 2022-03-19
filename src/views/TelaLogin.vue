a00b61bec31a04c3ce6ed936ad5671d774251c7f
Diego Resende <wdresende@gmail.com>
2022-03-19 15:06:26 -0300

    Adicionado página de login com as modificações

--- src/views/TelaLogin.vue
+++ src/views/TelaLogin.vue
@@ -1,90 +1,76 @@
 <template>
-<v-container class="mx-auto my-14">
-    <v-form
-        ref="form"
-        v-model="valid"
-        lazy-validation
-        >
-        <v-text-field
-            v-model="name"
-            :counter="10"
-            :rules="nameRules"
-            label="Name"
-            required
-        ></v-text-field>
+    <v-app id="inspire">
+    <v-main >
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1
+                          class="text-center display-1"
+                        >Faça seu login</h1>
+                        <v-form>
+                          <v-text-field
+                            label="Email"
+                            name="Email"
+                            type="text"
+                            color="teal accent-3"
+                          />
 
-        <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-        ></v-text-field>
-
-        <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="validate"
-        >
-            Validate
-        </v-btn>
-
-        <v-btn
-            color="error"
-            class="mr-4"
-            @click="reset"
-        >
-            Reset Form
-        </v-btn>
-
-        <v-btn
-            color="warning"
-            @click="resetValidation"
-        >
-            Reset Validation
-        </v-btn>
-    </v-form>
-</v-container>
+                          <v-text-field
+                            id="senha"
+                            label="Senha"
+                            name="password"
+                            type="password"
+                            color="teal accent-3"
+                          />
+                        </v-form>
+                      </v-card-text>
+                      <div class="text-center mt-3">
+                        <v-btn rounded color="primary" dark>Logar</v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" md="4">
+                      <v-card-text class="mt-12 mb-12 text-cadastro">
+                        <h1 class="text-center display-1" >Você não possui um cadastro?</h1>
+                        <h5
+                          class="text-center text-cadastrar mt-12 title"
+                        >Entre com seus dados pessoais e cadastre seus produtos</h5>
+                      </v-card-text>
+                      <div class="text-center btn-cadastrar">
+                        <v-btn rounded outlined dark @click="step++" color="primary">Cadastre-se</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
-    }),
-
-    methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
-    },
-  }
+    export default {
+        data: () => ({
+            step: 1
+        }),
+        props: {
+            source: String
+        }
+    };
 </script>
 
 <style scoped>
+
+    .btn-cadastrar {
+        margin-bottom: 10px;
+    }
 
 </style>
