29473f11f788e277a14327e695daed7b4a45b135
Matheus2106 <mate.santos.escolar@gmail.com>
2022-03-19 13:15:55 -0300

    Atualização do formulário do vendedor (caminhos)

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Inicio from '../views/Inicio.vue'
 import MeusProdutos from '../views/MeusProdutos.vue'
+import formDoVendedor from '../components/formDoVendedor.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,12 @@ const routes = [
     path: '/produtos',
     name: 'Produtos',
     component: MeusProdutos
+  },
+
+  {
+    path: '/formulario',
+    name: 'Formulario',
+    component: formDoVendedor
   }
 ]
 
